1171352fc95f145c42b8629dc611824ab6c84313
疯狂减肥带 <ureygt@gmail.com>
2016-10-31 18:46:23 +0800

    取消settimeout机制

--- acfun.js
+++ acfun.js
@@ -3,57 +3,47 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'password',
-  database : 'database'
+  password : '',
+  database : 'acfun' //数据库名
 });
-var web='http://www.acfun.tv/usercard.aspx?uid='
-//开始ID
-var start=549600
-//爬取的数量 建议一次不要太大
-var number=200000
-//每次爬取间隔时间
-var time=100
+var web='http://www.aixifan.com/usercard.aspx?uid='
+//start 为开始用户uid  number指用户数量 timefrequency为每次请求时间间隔
+var start=0
+var number=70*10000
+var timefrequency=10
 var end=start+number
 connection.connect();
-for(var i=0;i<=number;i++){
-  setTimeout(getjsonx,time);
-}
-
-function getjsonx(){
-  start++
-  if (start==end) { 
-   console.log('completed!!')
-  connection.end()
-  }
+var myInterval=setInterval(getjson,10,start)
+function getjson(){
+var nowuid=start
+//这里偷懒了下 直接在请求到指定数量后5s关闭链接
+if (nowuid>=end) {clearTimeout(myInterval);setTimeout(function(){console.log("completed!!");connection.end()},5000)}
+//http.get请求
 http.get(web+start, function(res) {
   var jsonx=''
   res.on('data', function(data) {
    jsonx+=data
   })
  res.on('end',function(){
-  jsony=JSON.parse(jsonx)
-  //if判断是否成功
+  jsony=JSON.parse(jsonx);
   if (jsony.success) {
-    console.log('success')
-    acfunuser=jsony.userjson 
-    //执行mysql操作函数 database请替换数据库名称 user请替换表名称
-    connection.query("INSERT INTO `database`.`user` (`uid`, `name`, `level`, `currExp`, `sign`, `posts`, `comments`, `follows`, `fans`, `lastLoginDate`, `regTime`, `comeFrom`, `views`) VALUES ("+acfunuser.uid+",'"+acfunuser.name+"',"+ acfunuser.level+","+ acfunuser.currExp+",'"+ acfunuser.sign+"',"+acfunuser.posts+", "+acfunuser.comments+","+acfunuser.follows+","+acfunuser.fans+",'"+acfunuser.lastLoginDate+"','"+acfunuser.regTime+"','"+acfunuser.comeFrom+"',"+ acfunuser.views+")", function(err, rows, fields) {
+    acfunuser=jsony.userjson
+    //下面的acfun为 database 为tablename
+    connection.query("INSERT INTO `acfun`.`user5` (`uid`, `name`, `level`, `currExp`, `sign`, `posts`, `comments`, `follows`, `fans`, `lastLoginDate`, `regTime`, `comeFrom`, `views`) VALUES ("+acfunuser.uid+",'"+acfunuser.name+"',"+ acfunuser.level+","+ acfunuser.currExp+",'"+ acfunuser.sign+"',"+acfunuser.posts+", "+acfunuser.comments+","+acfunuser.follows+","+acfunuser.fans+",'"+acfunuser.lastLoginDate+"','"+acfunuser.regTime+"','"+acfunuser.comeFrom+"',"+ acfunuser.views+")", function(err, rows, fields) {
       //if (err) throw err;
     console.log('uid: '+acfunuser.uid+'  mysql write success');
      });
   }
   else{
-    //用户不存在啦
-    console.log('eero user not found')
+     console.log('uid: '+nowuid+'  eero user not found')
   }
  })
 
+ 
+
 }).on('error', function(e) {
-  //网络错误 没有获取到数据
   console.error(e);
   console.log('eero web server not found')
 })
-
-
-
+start++
 }
